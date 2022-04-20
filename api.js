3cb533e7d72549591555d8361269377892e9aed8
Proxfam <72892893+Proxfam@users.noreply.github.com>
2022-04-20 19:00:49 +1200

    Fix

--- api.js
+++ api.js
@@ -1,1 +1,8 @@
-console.log("cock");
+url = `http://192.168.1.80/api/VZbaIYHIkNqgInHul20P6Ju2j62XO3jxlrApA4hZ/lights/1/state`;
+try {
+    await axios.put(url, {
+        on: true,
+    });
+} catch (err) {
+    console.error(err);
+}
