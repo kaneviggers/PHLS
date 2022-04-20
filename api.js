ff44bdf6a570d586b8e36f403ebf379eaf68b099
Proxfam <72892893+Proxfam@users.noreply.github.com>
2022-04-20 19:02:55 +1200

    Fix

--- api.js
+++ api.js
@@ -1,6 +1,8 @@
+const axios = require('axios');
+
 url = `http://192.168.1.80/api/VZbaIYHIkNqgInHul20P6Ju2j62XO3jxlrApA4hZ/lights/1/state`;
 try {
-    await axios.put(url, {
+    axios.put(url, {
         on: true,
     });
 } catch (err) {
