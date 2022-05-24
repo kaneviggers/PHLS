a5eddadbdc96220ffdfd9871c3473ef9f7a478a7
CmacKenzie001 <104046376+CmacKenzie001@users.noreply.github.com>
2022-05-24 17:21:35 +1200

    disco mode added

--- api.js
+++ api.js
@@ -9,7 +9,6 @@ if (user == 'cam') {
     var URL = "http://192.168.1.66/api/VZbaIYHIkNqgInHul20P6Ju2j62XO3jxlrApA4hZ";
 }
 
-
 class hueLight {
     id;
     preset;
@@ -24,73 +23,90 @@ class hueLight {
         this.bri = bri;
         this.hue = hue;
 
+        console.log(typeof(preset));
+
+        console.log(preset);
+
         switch(preset) {
+            case 1:
             case "defaultWhite":
+                console.log('cock');
                 this.sat = 0,
                 this.bri = 255,
                 this.hue = 0,
                 console.log('defWhite') // Debug (remove later)
                 break
+            case 2:
             case "defaultWarm":
                 this.sat = 175,
                 this.bri = 255,
                 this.hue = 8000,
                 console.log('defWarm') // Debug (remove later)
                 break
+            case 3:
             case "defaultCold":
                 this.sat = 100,
                 this.bri = 255,
                 this.hue = 43000,
                 console.log('defCold') // Debug (remove later)
                 break
+            case 4:
             case "red":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 0,
                 console.log('red') // Debug (remove later)
                 break
+            case 5:
             case "orange":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 5000,
                 console.log('orange') // Debug (remove later)
                 break
+            case 6:
             case "yellow":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 9500,
                 console.log('yellow') // Debug (remove later)
                 break
+            case 7:
             case "green":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 25000,
                 console.log('green') // Debug (remove later)
                 break
+            case 8:
             case "teal":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 34000,
                 console.log('teal') // Debug (remove later)
                 break
+            case 9:
             case "blue":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 46000,
                 console.log('blue') // Debug (remove later)
                 break
+            case 10:
             case "purple":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 50000,
                 console.log('purple') // Debug (remove later)
                 break
+            case 11:
             case "magenta":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 55000,
                 console.log('magenta') // Debug (remove later)
                 break
+            case 12:
             case "pink":
                 this.sat = 255,
                 this.bri = 255,
@@ -105,7 +121,7 @@ var chooseScene = (scene) => {
     for (light in scenes[scene]) {
         try{
             axios.put(URL + `/lights/${scenes[scene][light].id}/state`, {
-                on: false,
+                on: true,
                 sat: scenes[scene][light].sat,
                 bri: scenes[scene][light].bri,
                 hue: scenes[scene][light].hue,
@@ -116,14 +132,39 @@ var chooseScene = (scene) => {
     }
 }
 
-function disco () {
-
-}
-
 let scenes = {
     "Default" : [
-        new hueLight(1, "red")
+        new hueLight(1, 8)
     ]
 }
 
-chooseScene("Default");
+function disco() {
+    var randColor = Math.round(Math.random() * 11) + 1
+    var setDiscoColor = new hueLight(1,randColor);
+    axios.put(URL + `/lights/${setDiscoColor.id}/state`, {
+        on: true,
+        sat: setDiscoColor.sat,
+        bri: setDiscoColor.bri,
+        hue: setDiscoColor.hue,
+    });
+
+}
+
+/*function candleLight() {                                                  working on this
+    var randColor = Math.round(Math.random() * 100) + 1
+    var setDiscoColor = new hueLight(1,randColor);
+    axios.put(URL + `/lights/${setDiscoColor.id}/state`, {
+        on: true,
+        sat: setDiscoColor.sat,
+        bri: setDiscoColor.bri,
+        hue: setDiscoColor.hue,
+    });
+
+}
+//setInterval(disco, 100);
+
+//chooseScene("Default");
+
+setInterval(candleLight, 1000)
+
+
