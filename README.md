5599ec179ea69ca9899ac16beaeb0aec50ed036b
CmacKenzie001 <104046376+CmacKenzie001@users.noreply.github.com>
2022-05-25 20:06:00 +1200

    faliure: not getting a duplicate random number.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # HueLightSystem
 Very cool Philips Hue Light System PHLS
+
+============CUSTOM CLASSES============
+    hueLight:
+    huelight is a class that creates instances of 3 variables used for displaying lights.
+    each case is assigned a name, and a number. 
+
+============CUSTOM FUNCTIONS==========
+
+    setLightState:
+    setLightState (lightID, onOrOff, lightColor)
+    setlightstate sends data via an HTTP request to the hue bridge. 
+    the provided data defines the light's brightness, hue and saturation.
+
+    - lightID is a integer used to specify what light will be affected
+    - onOrOff specifies if the light is on or off
+    - lightcolor gets the sat, bri and hue values of the library, "hueLight".
+
+
+    disco:
+    disco()
+    disco makes a specified light  display a random color. 
+    in unison with setInteval(), disco() can make a light flash random colors for a disco effect.
--- api.js
+++ api.js
@@ -25,84 +25,84 @@ class hueLight {
         console.log(preset); //debug, remove later
         //color library, every color has a name and a number value.
         switch(preset) {
-            case 1:
+            case 0:
             case "defaultWhite":
                 this.sat = 0,
                 this.bri = 255,
                 this.hue = 0,
                 console.log('defWhite') // Debug (remove later)
                 break
-            case 2:
+            case 1:
             case "defaultWarm":
                 this.sat = 175,
                 this.bri = 255,
                 this.hue = 8000,
                 console.log('defWarm') // Debug (remove later)
                 break
-            case 3:
+            case 2:
             case "defaultCold":
                 this.sat = 100,
                 this.bri = 255,
                 this.hue = 43000,
                 console.log('defCold') // Debug (remove later)
                 break
-            case 4:
+            case 3:
             case "red":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 0,
                 console.log('red') // Debug (remove later)
                 break
-            case 5:
+            case 4:
             case "orange":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 5000,
                 console.log('orange') // Debug (remove later)
                 break
-            case 6:
+            case 5:
             case "yellow":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 9500,
                 console.log('yellow') // Debug (remove later)
                 break
-            case 7:
+            case 6:
             case "green":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 25000,
                 console.log('green') // Debug (remove later)
                 break
-            case 8:
+            case 7:
             case "teal":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 34000,
                 console.log('teal') // Debug (remove later)
                 break
-            case 9:
+            case 8:
             case "blue":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 46000,
                 console.log('blue') // Debug (remove later)
                 break
-            case 10:
+            case 9:
             case "purple":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 50000,
                 console.log('purple') // Debug (remove later)
                 break
-            case 11:
+            case 10:
             case "magenta":
                 this.sat = 255,
                 this.bri = 255,
                 this.hue = 55000,
                 console.log('magenta') // Debug (remove later)
                 break
-            case 12:
+            case 11:
             case "pink":
                 this.sat = 255,
                 this.bri = 255,
@@ -112,50 +112,33 @@ class hueLight {
         }
     }
 }
+2
+function disco() { //more info about this function in README.md
 
-/*var chooseScene = (scene) => {
-    for (lightID in scenes[scene]) {
-        try{
-            axios.put(URL + `/lights/${scenes[scene][lightID].id}/state`, {
-                on: true,
-                sat: scenes[scene][lightID].sat,
-                bri: scenes[scene][lightID].bri,
-                hue: scenes[scene][lightID].hue,
-            });
-        } catch(err) {
-            console.error(err);
+    var randColor = Math.round(Math.random() * 11) + 1
+    var redo = true
+    while (redo == true){ 
+        try {
+            if (referenceColor == randColor) {
+                var randColor = Math.round(Math.random() * 11) + 1
+            } else{
+                setLightState(1, true, new hueLight(randColor));
+                var referenceColor = randColor
+            }
+        } catch (error) {
+            referenceColor = 0
+            
         }
-    }
-}
-*/
-/*let scenes = {
-    "Default" : [new hueLight('green')]
-}*/
-
-/*function disco() {
-    var randColor = Math.round(Math.random() * 11) + 1
-    var setDiscoColor = new hueLight(1,randColor);
-    axios.put(URL + `/lights/${setDiscoColor.id}/state`, {
-        on: true,
-        sat: setDiscoColor.sat,
-        bri: setDiscoColor.bri,
-        hue: setDiscoColor.hue,
-    });
-
-}*/
-function disco() {
-    var randColor = Math.round(Math.random() * 11) + 1
-    var setDiscoColor = new hueLight(randColor);
-    setLightState(1, true, randColor);
+    }       
 }
 
- 
-function setLightState(lightId, onOff, color) {
+
+function setLightState(lightId, onOff, color) {  //more info about this function in README.md
     try {
         axios.put(`${URL}/lights/${lightId}/state`, { 
             on: onOff, 
             sat: color.sat, 
-            bri: color.bri,
+            bri: color.bri,    
             hue: color.hue
         });
     } catch (err) {
@@ -165,8 +148,12 @@ function setLightState(lightId, onOff, color) {
 
 }
 
-//setLightState(1, true, new hueLight('pink'))
-setInterval(disco, 500)
+
+
+
+
+//setLightState(1, true, new hueLight('orange'))
+setInterval(disco, 1000)
     
 
 
